8533b4427026977d9f4140a50c9ecaf4e0dc9a2c
dipali <110379398+dipalishirsath@users.noreply.github.com>
2022-08-08 14:34:15 +0530

    Update Demo.java

--- Ne_Git/src/Demo.java
+++ Ne_Git/src/Demo.java
@@ -1,6 +1,11 @@
 
-public class Demo {
+public class Demo 
+{
+	public void m1()
+	{
+      System.out.println("Hello Git");
+	}
 public static void main(String[] args) {
-	System.out.println("Hello Git");
+	System.out.println("Bye Git");
 }
 }
